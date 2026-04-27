d2db9d11c77228cb14e4566d01780b1cf6ac7ce8
brayansantanasilva2020 <brayansantanasilva2020@gmail.com>
2026-04-27 16:01:14 +0000

    versao 1

--- vor_sentinela.py
+++ vor_sentinela.py
@@ -1,1 +1,6 @@
-
+Exemplo de uso da Variavel Sentinela
+while true:
+    comando = input ("Digite um comando-Para parar Digite 'sair'")
+if comando == "sair":
+    break
+print (f"Executando:{comando}")
