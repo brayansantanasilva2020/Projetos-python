a2b7d547a949dc392cc1900115b589fd991ea0ad
brayansantanasilva2020 <brayansantanasilva2020@gmail.com>
2026-05-04 09:44:16 -0300

    v1

---
+++ seletor.py
@@ -0,0 +1,8 @@
+#--Seletor de Caracteres--
+letra = input("Digite a letra")
+if letra == "A":
+  print(Você escolheu a letra"A")
+elif letra == "B":
+  print ("Você escolheu a letra "B")
+else:
+  print ("Essa opçâo não esta Disponivel")
