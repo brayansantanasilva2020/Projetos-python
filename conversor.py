767ee6a158b8bd1c2a43bde9172ba17006087638
brayansantanasilva2020 <brayansantanasilva2020@gmail.com>
2026-05-25 13:06:53 +0000

    v1

--- conversor.py
+++ conversor.py
@@ -1,9 +1,9 @@
 #ferramenta de Conversão Dolar x Real--
-def converter (valor_dolar):
-    taxa = 5,15
-    valor_real = valor_dolar *taxa
+def converter(valor_dolar):
+    taxa = 5.15
+    valor_real = valor_dolar * taxa
     return valor_real
-print ("Conversor dolar e real")
-preco = float(input("Digite o preço do produto em Dólar:"))
+print("Conversor dolar x real")
+preço = float(input("Digite o preço do produto em Dólar:"))
 resultado = converter(preco)
-print(f"O valor em reais é:{resultado:.2f}")
+print(f"O valor em reais é: {resultado}:.2f}")
