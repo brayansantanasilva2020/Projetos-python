b5f0622eeed3a61b8b139960b4d8db2846ad060e
brayansantanasilva2020 <brayansantanasilva2020@gmail.com>
2026-05-25 13:03:02 +0000

    v1

---
+++ cabecalho.py
@@ -0,0 +1,10 @@
+### Definindo e Chamando a Função
+def exibir_cabecalho():
+ print('=' * 30)
+ print('SISTEMA DE GESTAO ESCOLAR')
+ print('Versão 1.0 - Desenvolvido no Codespaces')
+ print('=' * 30)
+
+### Chamando a função
+exibir_cabecalho()
+print('Menu Principal:')
---
+++ conversor.py
@@ -0,0 +1,9 @@
+#ferramenta de Conversão Dolar x Real--
+def converter (valor_dolar):
+    taxa = 5,15
+    valor_real = valor_dolar *taxa
+    return valor_real
+print ("Conversor dolar e real")
+preco = float(input("Digite o preço do produto em Dólar:"))
+resultado = converter(preco)
+print(f"O valor em reais é:{resultado:.2f}")
