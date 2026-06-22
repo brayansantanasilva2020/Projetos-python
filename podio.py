12ca83ba942c31cdc2ccf06aa1a34f4a4c7ffa90
brayansantanasilva2020 <brayansantanasilva2020@gmail.com>
2026-06-22 14:21:00 +0000

    v1

---
+++ podio.py
@@ -0,0 +1,8 @@
+# Lista com os vencedores
+
+vencedores = ["Leonardo", "Ravy", "Breno"]
+
+# Exibindo o podio
+print("Top 1:", vencedores[0])
+print("Top 2:", vencedores[1])
+print("Top 3:", vencedores[2])
