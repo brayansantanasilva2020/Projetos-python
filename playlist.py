8944442d7012f902e30377c2f75f74c83bc1425f
brayansantanasilva2020 <brayansantanasilva2020@gmail.com>
2026-06-22 13:02:39 +0000

    v1

--- playlist.py
+++ playlist.py
@@ -1,6 +1,6 @@
 print("Music Player")
 playlist = ["Musica 1-rock", "Musica 2-POP", "Musica 3-RAP"]
-atual = 0
-print(f"Tocando Agora: {playlist[atual]}")
+atual = int(input("Digite o numero da musica"))
+print(f"Tocando Agora: {playlist[indice]}")
 print("Musica Anterior:---(inicio da playlist)")
 print("=" *40)
